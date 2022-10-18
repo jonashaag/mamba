47badda4e9a9055696b7ef65910ffe304efa1e91
Jonas Haag <jonas@lophus.org>
2022-10-18 10:11:54 +0200

    Increase number of parallel curl connections

--- libmamba/include/mamba/core/context.hpp
+++ libmamba/include/mamba/core/context.hpp
@@ -137,7 +137,7 @@ namespace mamba
         ChannelPriority channel_priority = ChannelPriority::kFlexible;
         bool auto_activate_base = false;
 
-        std::size_t download_threads = 5;
+        std::size_t download_threads = 50;
         int extract_threads = 0;
 
         int verbosity = 0;
